6b0fbe93aeb2822b52656292e7fe00d461508db6
qkmBit <d.r.solovjev@ya.ru>
2022-02-27 05:35:59 +0300

    Добавьте файлы проекта.

---
+++ rgd/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using rgd.main;
+using System.Windows.Forms;
+
+namespace rgd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Main main = new Main();
+            textBox2.Text = main.Translate(textBox1.Text);
+
+        }
+    }
+}
---
+++ rgd/Main.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rgd.main
+{
+    class Main
+    {
+        Dictionary<char, string> alphabet = new Dictionary<char, string>()
+        {
+            {'а',"a" },
+            {'б',"b"},
+            {'в',"v" },
+            {'г',"g" },
+            {'д',"d" },
+            {'е',"e" },
+            {'ё',"e" },
+            {'ж',"zh" },
+            {'з',"z" },
+            {'и',"i" },
+            {'й',"i" },
+            {'к',"k" },
+            {'л',"l" },
+            {'м',"m" },
+            {'н',"n" },
+            {'о',"o" },
+            {'п',"p" },
+            {'р',"r" },
+            {'с',"s" },
+            {'т',"t" },
+            {'у',"u" },
+            {'ф',"f" },
+            {'х',"kh" },
+            {'ц',"ts" },
+            {'ч',"ch" },
+            {'ш',"sh" },
+            {'щ',"shch" },
+            {'ы',"y" },
+            {'ъ',"ie" },
+            {'э',"e" },
+            {'ю',"iu" },
+            {'я',"ia" },
+            {'А',"A" },
+            {'Б',"B"},
+            {'В',"V" },
+            {'Г',"G" },
+            {'Д',"D" },
+            {'Е',"E" },
+            {'Ё',"E" },
+            {'Ж',"Zh" },
+            {'З',"Z" },
+            {'И',"I" },
+            {'Й',"I" },
+            {'К',"K" },
+            {'Л',"L" },
+            {'М',"M" },
+            {'Н',"N" },
+            {'О',"O" },
+            {'П',"P" },
+            {'Р',"R" },
+            {'С',"S" },
+            {'Т',"T" },
+            {'У',"U" },
+            {'Ф',"F" },
+            {'Х',"Kh" },
+            {'Ц',"Ts" },
+            {'Ч',"Ch" },
+            {'Ш',"Sh" },
+            {'Щ',"Shch" },
+            {'Э',"E" },
+            {'Ю',"Iu" },
+            {'Я',"Ia" },
+            {'ь',"" },
+            {' '," " }
+        };
+
+        public Dictionary<char, string> Alphabet { get => alphabet; }
+        public string Translate(string str)
+        {
+            string transl = "";
+            foreach(char c in str)
+            {
+                if (alphabet.ContainsKey(c)) transl += alphabet[c];
+                else transl += c;
+            }
+            return transl;
+        }
+    }
+}
